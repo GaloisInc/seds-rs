6a5d248159747c3048f1e7503d0ff030c3996c8b
Ethan Lew <elew@galois.com>
2023-08-10 12:47:05 -0700

    remove context (for now)

--- src/codegen/context.rs
+++
@@ -1,9 +0,0 @@
-
-/// CodeGenContext tracks the state of the code generation process. 
-/// This resolves type references and generate validation traits.
-/// NOTE: maybe a type checker should be implemented between this and the AST generation.
-pub struct CodeGenContext {
-    types: HashMap<String, RustType>,
-    validation_rules: HashMap<String, Vec<ValidationRule>>,
-}
-
