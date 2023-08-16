24fd3aae196ee9095f60996bbc3ff450993a9c82
Ethan Lew <elew@galois.com>
2023-08-16 11:47:43 -0500

    add a test to codegen cFE

--- tests/codegen.rs
+++ tests/codegen.rs
@@ -58,3 +58,25 @@ fn test_spacepacket() {
     let mut file = File::create("test_output/spacepacket.rs").unwrap();
     file.write_all(code.as_bytes()).unwrap();
 }
+
+
+/// test against the supported files in the cFE
+#[test]
+fn test_cfe() {
+    let contents = open_file("eds/cFE/modules/core_api/eds/base_types.xml");
+    let rpf: raw::PackageFile = serde_xml_rs::from_str(&contents).unwrap();
+
+    let ectx = get_mission_params();
+    let pf = rpf.resolve(&ectx).unwrap();
+
+    let code = rustfmt(pf.to_rust_mod(None).unwrap()).unwrap();
+    println!("{}", code);
+
+    // write to a tmp file
+    use std::fs::File;
+    use std::io::Write;
+    // make sure the directory exists and make it if not
+    std::fs::create_dir_all("test_output").unwrap();
+    let mut file = File::create("test_output/spacepacket.rs").unwrap();
+    file.write_all(code.as_bytes()).unwrap();
+}
