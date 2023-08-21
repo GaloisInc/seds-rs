f0cab7f60bdc04f469eb82dac372f7c4b88a0a80
Ethan Lew <elew@galois.com>
2023-08-21 10:15:14 -0500

    add codegen test for supported portion of cFE (comment our paths to unsupported files)

--- seds_macro/src/lib.rs
+++ seds_macro/src/lib.rs
@@ -2,15 +2,13 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use proc_macro2;
 use syn::{parse_macro_input, AttributeArgs, NestedMeta, Lit, Meta};
 
 use seds_rs::eds::raw;
 use seds_rs::eds::ast;
 use seds_rs::expr::ExpressionContext;
 use seds_rs::eds::resolve::Resolve;
-use seds_rs::codegen::ToRustMod;
-use seds_rs::codegen::context::{Namespace, CodegenContext};
+use seds_rs::codegen::codegen_packagefiles;
 
 #[proc_macro_attribute]
 pub fn seds(attr: TokenStream, _item: TokenStream) -> TokenStream {
@@ -33,7 +31,6 @@ pub fn seds(attr: TokenStream, _item: TokenStream) -> TokenStream {
         }
     }
 
-    let mut generated_code = proc_macro2::TokenStream::new();
     let mut package_files = Vec::<ast::PackageFile>::new();
     for xml_file in xml_files.iter() {
         // Check if file exists
@@ -72,58 +69,11 @@ pub fn seds(attr: TokenStream, _item: TokenStream) -> TokenStream {
         package_files.push(pf);
     } 
 
-    for xml_file in xml_files.iter() {
-        // Check if file exists
-        let path = std::path::Path::new(xml_file);
-        if !path.exists() {
-            panic!("File {} does not exist", xml_file);
-        }
-
-        // Read the file content
-        let file_content = std::fs::read_to_string(xml_file)
-            .expect(&format!("Failed to read the file: {}", xml_file));
-
-        // Parse the XML content
-        let rpf: raw::PackageFile = serde_xml_rs::from_str(&file_content)
-            .expect(&format!("Failed to parse the file: {}", xml_file));
-
-        let ectx = if let Some(params_file) = &parameters {
-            let params_content = std::fs::read_to_string(params_file)
-                .expect(&format!("Failed to read the parameters file: {}", params_file));
-            
-            let json = serde_json::from_str(&params_content.as_str())
-                .expect(&format!("Failed to parse the parameters file: {}", params_file));
-
-            ExpressionContext::from_json(&json)
-                .expect(format!("Failed to parse the parameters file {}", params_file).as_str())
-        } else {
-            let json = serde_json::json!({});
-            ExpressionContext::from_json(&json).expect("Failed to parse the default parameters")
-        };
-
-        let pf_result = rpf.resolve(&ectx);
-        if let Err(e) = pf_result {
-            panic!("Failed to resolve the package file: {:?}", e);
-        }
-        let pf = pf_result.unwrap(); 
-
-        // Generate Rust code
-        let pfs: Vec<&ast::PackageFile> = package_files.iter().collect();
-        let namespace = Namespace::try_from(pfs).unwrap();
-        for pkg in pf.package.iter() {
-            let locals = Namespace::try_from(pkg).unwrap();
-            let ctx = CodegenContext {
-                name: None,
-                locals: &locals,
-                namespace: &namespace,
-            };
-            let code_result = pf.to_rust_mod(&ctx);
-            if let Err(e) = code_result {
-                panic!("Failed to generate Rust code: {:?}", e);
-            }
-            generated_code.extend(code_result.unwrap());
-        }
+    // Generate Rust code
+    let pfs: Vec<&ast::PackageFile> = package_files.iter().collect();
+    let gc_result = codegen_packagefiles(&pfs);
+    if let Err(e) = gc_result {
+        panic!("Codegen Error: {:?}", e);
     }
-    
-    generated_code.into()
+    gc_result.unwrap().into()
 }
---
+++ tests/codegen.rs
@@ -0,0 +1,48 @@
+use common::{get_mission_params, open_file};
+use seds_rs::codegen::codegen_packagefiles;
+use seds_rs::eds::ast::PackageFile;
+use seds_rs::eds::raw;
+use seds_rs::{codegen::rustfmt, eds::resolve::Resolve};
+
+mod common;
+
+/// test the namespace on multiple eds files
+#[test]
+fn test_cfe_codegen() {
+    let paths = vec![
+        "eds/cFE/modules/core_api/eds/cfe_fs.xml",
+        "eds/cFE/modules/core_api/eds/ccsds_spacepacket.xml",
+        "eds/cFE/modules/core_api/eds/base_types.xml",
+        "eds/cFE/modules/fs/eds/cfe_fs.xml",
+        "eds/cFE/modules/resourceid/eds/cfe_resourceid.xml",
+        // "eds/cFE/modules/sb/eds/cfe_sb.xml", // PaddingEntry is not yet supported
+        // "eds/cFE/modules/evs/eds/cfe_evs.xml", // PaddingEntry is not yet supported
+        // "eds/cFE/modules/tbl/eds/cfe_tbl.xml", // PaddingEntry is not yet supported
+        // "eds/cFE/modules/time/eds/cfe_time.xml", // 1HzCmd is not a valid Ident
+        // "eds/cFE/modules/cfe_testcase/eds/cfe_testcase.xml", // depends on CFE_HDR
+        // "eds/cFE/modules/core_private/eds/base_types.xml", // InvalidBitSize(8192)
+        // "eds/cFE/modules/es/eds/cfe_es.xml", // Unsupported ArrayDataType
+    ];
+    let rpackagefiles: Vec<raw::PackageFile> = paths
+        .iter()
+        .map(|fp| serde_xml_rs::from_str(&open_file(fp)).unwrap())
+        .collect();
+
+    let ectx = get_mission_params();
+    let packagefiles: Vec<PackageFile> = rpackagefiles
+        .iter()
+        .map(|rpf| rpf.resolve(&ectx).unwrap())
+        .collect();
+    let pfs: Vec<&PackageFile> = packagefiles.iter().collect();
+
+    let code_tokens = codegen_packagefiles(&pfs).unwrap();
+    let code = rustfmt(code_tokens).unwrap();
+
+    // write to a tmp file
+    use std::fs::File;
+    use std::io::Write;
+    // make sure the directory exists and make it if not
+    std::fs::create_dir_all("test_output").unwrap();
+    let mut file = File::create("test_output/cfe.rs").unwrap();
+    file.write_all(code.as_bytes()).unwrap();
+}
